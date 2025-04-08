d25fe14775f07c0839cbd5433cc2a0f440f00a33
S-Sharma007 <shubhamsharma95363@gmail.com>
2025-04-08 23:01:53 +0530

    EKS node and scaling group

--- Terraform/module/EKS/main.tf
+++ Terraform/module/EKS/main.tf
@@ -18,3 +18,63 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSCLusterPolicy" {
     role       = aws_iam_role.cluster.name
 }
 
+resource "aws_eks_cluster" "main" {
+  name = var.cluster_name
+  role_arn = aws_iam_role.cluster.arn
+    version = var.cluster_version
+    vpc_config {
+        subnet_ids = var.subnet_ids   
+    }
+depends_on = [ 
+    aws_iam_role_policy_attachment.cluster_AmazonEKSCLusterPolicy
+ ]
+
+}
+
+
+resource "aws_iam_role" "node"{
+    name = "${var.cluster_name}-node-role"
+ assume_role_policy = jsondecode(({
+    Version = "2012-10-17"
+    Statement = [
+        "sts:AssumeRole",
+        "sts: TagSession"
+    ]
+    Effect = "Allow"
+    Principal = {
+        Service = "ec2.amazonaws.com"
+    }
+ }))   
+}
+
+resource "aws_iam_role_policy_attachment" "node_AmazonEKSWorkerNodePolicy" {
+    for_each = toset ([
+        "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+        "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+        "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
+    ])
+    policy_arn = each.value
+    role       = aws_iam_role.node.name
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = each.key
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.subnet_ids
+
+  instance_types = each.value.instance_type
+capacity_type  = each.value.capacity_type
+
+  scaling_config {
+    desired_size = each.value.scaling_config.desired_size
+    max_size     = each.value.scaling_config.max_size
+    min_size     = each.value.scaling_config.min_size
+  }
+
+  
+ depends_on = [
+    aws_iam_role_policy_attachment.node_policy
+     ]
+
+}
